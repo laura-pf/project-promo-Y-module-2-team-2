02c12d3ed8c2ad7912c6650dc9ae44de5792a38c
Laura Parra <laupafer93@gmail.com>
2024-06-27 00:16:32 +0200

    js comment

--- src/js/card.js
+++ src/js/card.js
@@ -9,6 +9,8 @@ nameInput.addEventListener("input",(event)=>{
 namePreview.innerHTML = event.target.value;
 })
 
+//este evento funciona cuando se escribe un nombre en el input del formulario, ya que la carta cambia, pero habria que hacer algo para que cuando la usuaria borre lo que ha escrito, vuelva el valor que estaba por defecto
+
 occupationInput.addEventListener("input",(event)=>{
 occupationPreview.innerHTML = event.target.value;
 })
