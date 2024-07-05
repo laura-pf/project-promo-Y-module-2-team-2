8ae4ea834d13be3b4578911a1991e8e63c566cdb
Laura Parra <laupafer93@gmail.com>
2024-07-05 12:31:29 +0200

    add branch and js colors palete

---
+++ src/js/colors-palete.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Cuando la usuaria haga click en la paleta 2/paleta3, recojo valor de los inputs y se muestran los colores en: titulo e iconos, borde cartaS, y borde iconos*/
+
+const paleteKrusty = document.querySelector(".js-color-krusty");
+const paleteApu = document.querySelector(".js-color-apu");
+
+
+const clickPaleteKrusty = (event) => {
+    
+}
+
+paleteKrusty.addEventListener("click", clickPaleteKrusty);
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import "./card.js";
+import"./colors-palete.js";
 import "./desplegable.js";
 import "./addImage.js";
 import "./resetButton.js";
